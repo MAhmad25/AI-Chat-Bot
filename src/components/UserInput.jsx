74db82fd7839e727a57fcb87e4e5a346224e145d
Muhammad Ahmad <ahmadlatifshadow@gmail.com>
2025-06-07 01:10:10 +0500

    refactor design parameters and update user input placeholder for improved clarity

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -6,7 +6,7 @@ const UserInput = () => {
       const { userPrompt, setUserPrompt, generateMessage } = useContext(ChatPodia);
       return (
             <section className="w-full sm:mx-auto sm:w-1/2 py-3 sm:py-0 flex justify-around gap-2 px-2 items-center ">
-                  <input value={userPrompt} onChange={(e) => setUserPrompt(e.target.value)} className="w-full border-none caret-white backdrop-blur-md text-lg leading-none bg-[#22808A]/40  appearance-none  outline-none rounded-full py-4 px-5" type="text" required placeholder="Ask me anything..." />
+                  <input value={userPrompt} onChange={(e) => setUserPrompt(e.target.value)} className="w-full border-none caret-white backdrop-blur-md text-lg leading-none bg-[#22808A]/40  appearance-none  outline-none rounded-full py-4 px-5" type="text" required placeholder="Ask me anything small..." />
                   <button
                         onClick={() => {
                               generateMessage();
