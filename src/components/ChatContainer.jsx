4d4ca0ce30ec96204b0b80d16a98ea761de07a3d
Muhammad Ahmad <ahmadlatifshadow@gmail.com>
2025-06-07 23:58:13 +0500

    Refactor ChatContainer component for improved JSX syntax and layout consistency

--- src/components/ChatContainer.jsx
+++ src/components/ChatContainer.jsx
@@ -1,7 +1,6 @@
 import { useContext } from "react";
 import { ChatPodia } from "../contexts/Context";
 import toast from "react-hot-toast";
-
 const ChatContainer = () => {
       const { chatResponse, promptDiv, loading, error } = useContext(ChatPodia);
       if (error) {
@@ -16,18 +15,18 @@ const ChatContainer = () => {
                         </div>
                   ) : null}
                   {loading ? (
-                        <div class="relative flex  w-64 animate-pulse gap-2 p-4">
-                              <div class="h-12 w-12 rounded-full bg-white/40"></div>
-                              <div class="flex-1">
-                                    <div class="mb-1 h-5 w-3/5 rounded-lg bg-white/40 text-lg"></div>
-                                    <div class="h-5 w-[90%] rounded-lg bg-white/40 text-sm"></div>
+                        <div className="relative flex  w-64 animate-pulse gap-2 p-4">
+                              <div className="h-12 w-12 rounded-full bg-white/40"></div>
+                              <div className="flex-1">
+                                    <div className="mb-1 h-5 w-3/5 rounded-lg bg-white/40 text-lg"></div>
+                                    <div className="h-5 w-[90%] rounded-lg bg-white/40 text-sm"></div>
                               </div>
-                              <div class="absolute bottom-5 right-0 h-4 w-4 rounded-full bg-white/40"></div>
+                              <div className="absolute bottom-5 right-0 h-4 w-4 rounded-full bg-white/40"></div>
                         </div>
                   ) : (
                         chatResponse && (
-                              <div className="w-fit min-w-full px-3 pt-3  h-fit bg-black/60 backdrop-blur-xl rounded-xl ">
-                                    <pre className="text-xs sm:text-xl tracking-tight text-white ">{chatResponse} </pre>
+                              <div className="w-fit min-w-full overflow-hidden px-3 pt-3   h-fit bg-black/60 backdrop-blur-xl rounded-xl ">
+                                    <pre className="text-xs sm:text-xl tracking-tight text-white ">{chatResponse}</pre>
                               </div>
                         )
                   )}
