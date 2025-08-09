ba15387e2901a369195cbee1ff655d777cb51b2c
Muhammad Ahmad <ahmadlatifshadow@gmail.com>
2025-08-09 15:59:12 +0500

    Added Preloader

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Background from "./components/Background";
+import Loader from "./components/Design/Loader/Loader";
 import Main from "./components/Main";
 import { Toaster } from "react-hot-toast";
 
 const App = () => {
       return (
             <main className="relative text-white w-full h-dvh">
+                  <Loader />
                   <Background />
                   <Main />
                   <Toaster />
--- src/components/ChatContainer/ChatContainer.jsx
+++ src/components/ChatContainer/ChatContainer.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { ChatPodia } from "../../contexts/Context";
 import toast from "react-hot-toast";
 import styles from "./loader.module.css";
@@ -7,8 +7,22 @@ const ChatContainer = () => {
       if (error) {
             toast.error(error);
       }
+      useEffect(() => {
+            let glowInTexts = document.querySelectorAll(".glowIn");
+            glowInTexts.forEach((glowInText) => {
+                  let letters = glowInText.textContent.split("");
+                  glowInText.textContent = "";
+                  letters.forEach((letter, i) => {
+                        let span = document.createElement("span");
+                        span.textContent = letter;
+                        span.style.animationDelay = `${i * 0.05}s`;
+                        glowInText.append(span);
+                  });
+            });
+      }, [chatResponse]);
+
       return (
-            <section className="w-full sm:container sm:w-1/2 sm:mx-auto font-Nova [&::-webkit-scrollbar]:hidden h-3/4 overflow-y-scroll">
+            <section className="w-full px-5 sm:container sm:w-1/2 sm:mx-auto font-Nova [&::-webkit-scrollbar]:hidden h-3/4 overflow-y-scroll">
                   {promptDiv ? (
                         <section className="w-full  flex justify-end h-fit">
                               <div className="w-fit sm:w-4/5  h-fit mb-2  flex justify-end items-end">
@@ -21,7 +35,7 @@ const ChatContainer = () => {
                   ) : (
                         chatResponse && (
                               <div className="w-fit min-w-full overflow-hidden px-3 py-3 h-fit bg-[#303030] mix-blend-difference  rounded-xl ">
-                                    <p className="text-sm break-words  sm:text-xl tracking-tight text-white ">{chatResponse}</p>
+                                    <p className="text-sm break-words  sm:text-xl tracking-tight text-white glowIn ">{chatResponse}</p>
                               </div>
                         )
                   )}
