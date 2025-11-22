e836baf7e46e70c673669d10a986617475e4d90e
MAhmad25 <userarenotproduct@gmail.com>
2025-11-22 21:30:30 +0500

    just added the picture

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,13 @@ import UserInput from "./UserInput";
 
 const Main = () => {
       return (
-            <section className="w-full flex flex-col items-center  h-full absolute top-0 bg-[#000000]/10  bg-[linear-gradient(0deg,_rgba(0,_0,_0,_0.79)_0%,_rgba(237,_221,_83,_0)_100%)] left-0 ">
+            <section
+                  style={{
+                        backgroundImage: "url('https://pub-940ccf6255b54fa799a9b01050e6c227.r2.dev/ruixen_moon_2.png')",
+                        backgroundAttachment: "fixed",
+                  }}
+                  className="w-full flex flex-col items-center  h-full absolute top-0 bg-[#000000]/10 bg-cover bg-center  bg-[linear-gradient(0deg,_rgba(0,_0,_0,_0.79)_0%,_rgba(237,_221,_83,_0)_100%)] left-0 "
+            >
                   <ChatContainer />
                   <UserInput />
             </section>
